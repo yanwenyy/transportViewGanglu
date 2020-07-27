2b4b0ab04e527776c5e8a8f5cf6814521d81ff58
yanwenyy <18738390058@163.com>
2020-07-27 17:15:10 +0800

    接口对接

--- js/view1.js
+++ js/view1.js
@@ -1,4 +1,6 @@
 $(function () {
+    var noImgPage=10,
+        imgPage=5;
     $("#page1").paging({
             total: 10,
             numberPage: 5
@@ -66,5 +68,93 @@ $(function () {
     });
     $(".img-pre").click(function () {
         $(this).addClass("out");
-    })
+    });
+
+    //场内运输
+    ajax_get("jinding/factory/car/list?pageNum=1&pageSize="+noImgPage+"&evnCarNum=&registTime=", function (data) {
+        $("#factoryPage").paging({
+                total: data.total,
+                numberPage: noImgPage
+            },
+            function(msg) {
+                //回调函数 msg为选中页码
+                ajax_get("jinding/factory/car/list?pageNum="+msg+"&pageSize="+noImgPage+"&evnCarNum=&registTime=", function (data) {
+                    factory(data)
+                });
+            });
+        factory(data)
+    });
+    function factory(data) {
+        var list=data.data,i=0,len=list.length,html='';
+        for(;i<len;i++){
+            var v=list[i];
+            html+=' <tr>\n' +
+                '<td>'+v.evnCarNum+'</td>\n' +
+                '<td>'+v.registTime+'</td>\n' +
+                '<td>'+v.vehicleNum+'</td>\n' +
+                '<td>'+v.engineNum+'</td>\n' +
+                '<td>'+v.emissionStand+'</td>\n' +
+                '<td><img title="点击查看大图" class="table-img" src="'+(http_url.url+'/jinding/showImg/'+v.carCheckList)+'" alt=""></td>\n' +
+                '<td><img title="点击查看大图" class="table-img" src="'+(http_url.url+'/jinding/showImg/'+v.drivinglLicense)+'" alt=""></td>\n' +
+                '</tr>'
+        }
+        $(".factoryData").html(html)
+    }
+
+    //非道路移动机械电子台账
+    ajax_get("/jinding/offroad/list?pageNum=1&pageSize="+noImgPage+"&produceTime=&evnProNum=", function (data) {
+        $("#offroadPage").paging({
+                total: data.total,
+                numberPage: noImgPage
+            },
+            function(msg) {
+                //回调函数 msg为选中页码
+                ajax_get("/jinding/offroad/list?pageNum="+msg+"&pageSize="+noImgPage+"&produceTime=&evnProNum=", function (data) {
+                    offroad(data)
+                });
+            });
+        offroad(data)
+    });
+    function offroad(data) {
+        var list=data.data,i=0,len=list.length,html='';
+        for(;i<len;i++){
+            var v=list[i];
+            html+=' <tr>\n' +
+                '<td>'+v.evnProNum+'</td>\n' +
+                '<td>'+v.produceTime+'</td>\n' +
+                '<td>'+v.emission+'</td>\n' +
+                '<td>'+v.emissionNum+'</td>\n' +
+                '<td>'+v.engineNum+'</td>\n' +
+                '</tr>'
+        }
+        $(".offroadData").html(html)
+    }
+
+    //汇总表
+    ajax_get("/jinding/sum/list?pageNum=1&pageSize="+noImgPage+"&monthTime=&dayTime=&materialsName=", function (data) {
+        $("#totalPage").paging({
+                total: 10,
+                numberPage: noImgPage
+            },
+            function(msg) {
+                ajax_get("/jinding/sum/list?pageNum="+msg+"&pageSize="+noImgPage+"&monthTime=&dayTime=&materialsName=", function (data) {
+                    total(data)
+                });
+            });
+        total(data)
+    });
+    function total(data){
+        var list=data.data,i=0,len=list.length,html='';
+        for(;i<len;i++){
+            var v=list[i];
+            html+=' <tr>\n' +
+                '<td>'+(v.materialsName||'')+'</td>\n' +
+                '<td>'+v.carWeigh+'</td>\n' +
+                '<td>'+(v.sumWeigh-v.carWeigh)+'</td>\n' +
+                '<td>'+v.sumWeigh+'</td>\n' +
+                '<td>'+((v.sumWeigh-v.carWeigh)/ v.sumWeigh).toFixed(2)*100+'%'+'</td>\n' +
+                '</tr>'
+        }
+        $(".totalData").html(html)
+    }
 });
